ddff6601e37e828d7161559712b5f01d8b25ee1d
Vitaliy Bykovets <vitaliy.bykovets@gmail.com>
2018-10-21 21:52:30 +0300

    Add link handlers

--- src/index.js
+++ src/index.js
@@ -7,8 +7,42 @@ export default class Conversion {
     this.options = mergeOptions(defaults, options);
     this.eventBus = new EventBus();
 
-    this.DOM = {};
-    this.DOM.body = document.getElementsByTagName('body')[0];
-    this.DOM.containerToInsert = this.DOM.body.querySelector(this.options.containerToInsert);
+    this.disabled = false;
+    this.hostName = location.hostname;
+
+    this.links = [];
+    this.oldLinks = [];
+
+    this.dom = {};
+  }
+
+  init() {
+    this.dom = this.initDom();
+  }
+
+  update(options = {}) {
+    this.options = mergeOptions(defaults, options);
+  }
+
+  on(event, handler) {
+    this.eventBus.on(event, handler);
+  }
+
+  disable() {
+    this.disabled = true;
+  }
+
+  enable() {
+    this.disabled = false;
+  }
+
+  initDom() {
+    let dom = {};
+    dom.body = document.getElementsByTagName('body')[0];
+    dom.bgBody = document.querySelector('.bg-page');
+    dom.header = document.getElementsByTagName('header')[0];
+    dom.overlay = this.DOM.body.querySelector(this.options.overlay);
+    dom.containerToInsert = this.DOM.body.querySelector(this.options.containerToInsert);
+    return dom;
   }
 }
